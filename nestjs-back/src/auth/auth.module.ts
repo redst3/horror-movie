abde7ef8a09fdbc1237725ccd1938ba4f14a0e68
Redas Steckys <redass95@outlook.com>
2025-04-02 15:57:09 +0300

    replaced process.env jwt from .env

--- nestjs-back/src/auth/auth.module.ts
+++ nestjs-back/src/auth/auth.module.ts
@@ -3,14 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3h' },
+      }),
     }),
   ],
   providers: [AuthService],
